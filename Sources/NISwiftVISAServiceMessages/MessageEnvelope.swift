c2c23119d6db66912f7b389ae50848731d847f62
seeaya <connorbarnesdenver@icloud.com>
2020-12-29 18:32:38 -0700

    Added return envelopes.

--- Sources/NISwiftVISAServiceMessages/MessageEnvelope.swift
+++ Sources/NISwiftVISAServiceMessages/MessageEnvelope.swift
@@ -5,9 +5,14 @@
 //  Created by Connor Barnes on 12/29/20.
 //
 
-import Foundation
+import CVISA
 
 public struct MessageEnvelope<M: Message>: Codable {
 	public var type: String
 	public var message: M
+}
+
+public struct ReturnEnvelope<M: Message>: Codable {
+	public var message: M
+	public var status: ViStatus
 }
